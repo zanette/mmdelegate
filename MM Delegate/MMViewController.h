dd3a1f60892772e012b17090a515c53f98075fc4
zanette <zeebabe@gmail.com>
2013-02-11 14:01:51 -0600

    Initial Commit

---
+++ MM Delegate/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  MM Delegate
+//
+//  Created by Zanette Sanders on 2/11/13.
+//  Copyright (c) 2013 Zanette Sanders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
